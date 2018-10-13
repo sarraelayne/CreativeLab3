f8ae07544be44cf5b7a2504a82f653fd20111dde
EC2 Default User <ec2-user@ip-172-31-37-125.ec2.internal>
2018-10-13 20:49:11 +0000

    adding functionality

--- recs.js
+++ recs.js
@@ -1,7 +1,22 @@
 angular.module('app', [])
-    .controller('mainCtrl', mainCtrl);
+    .controller('mainCtrl', mainCtrl)
+    .directive('recs', recsDirective);
     
     
 function mainCtrl ($scope) {
     
+    $scope.recsSearch = function(user) {
+        
+    }
+}
+
+function recsDirective () {
+    return {
+        template: (
+            '<div class="recs">' +
+                '<h4>{{user.band}}</h4>' + '<h4>{{user.movie}}</h4>' +
+                '<h5>{{user.recs}}</h5>' +
+            '</div>'
+        )
+    };
 }
