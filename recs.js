363c7154af94fa9f30e08a2b4688399cb196f1d3
EC2 Default User <ec2-user@ip-172-31-37-125.ec2.internal>
2018-10-18 23:24:47 +0000

    needs loop

--- recs.js
+++ recs.js
@@ -1,20 +1,3 @@
-/*var app = angular.module('app', [])
-    .controller('mainCtrl', mainCtrl);
-
-function mainCtrl($scope, $http) {
-    $scope.recsSearch = function(user) {
-        var baseURL = "https://tastedive.com/api/similar?k=321657-Angular2-0PYPIS84&verbose=1&q=";
-        var separator = "%2C";
-        var band = "The+Beatles"
-        var movie = "Lion+King"
-        //var band = user.band.replace(/ /g, '+');
-        //var movie = user.movie.replace(/ /g, '+');
-        //var completedURL = baseURL + band + separator + movie;
-        console.log("API endpoint: " + completedURL);
-        var completedURL = "https://tastedive.com/api/similar?k=321657-Angular2-0PYPIS84&verbose=1&q=The+Beatles%2CLion+King";
-    }
-}
-*/
 var app = angular.module('myApp', []);
 app.config(function($sceDelegateProvider) {
   $sceDelegateProvider.resourceUrlWhitelist(['**']);
@@ -22,10 +5,9 @@ app.config(function($sceDelegateProvider) {
 
 app.controller('myCtrl',
   function($scope, $http) {
-    console.log("outside scope");
+    $scope.recs = [];
     $scope.recsSearch = function(form) {
       console.log(form);
-      console.log("TasteDive");
       var baseURL = "https://tastedive.com/api/similar?k=321657-Angular2-0PYPIS84&verbose=1&q=";
       var separator = "%2C";
       var band = form.band.replace(/ /g, '+');
@@ -38,6 +20,36 @@ app.controller('myCtrl',
           console.log(response.data);
           console.log(response.data.Similar);
           console.log(response.data.Similar.Results);
+          var results = response.data.Similar.Results;
+          $scope.recs.push({
+            name: results[0].Name,
+            type: results[0].Type,
+            youtube: results[0].yUrl
+          });
+          console.log($scope.recs);
         });
-    };
+      };
+    $scope.clicked = function() {
+      console.log("in clicked");
+      $window.location.assign = $scope.recs[0].yUrl;
+    }
   });
+  
+app.directive('recs', 
+  function () {
+    return {
+        scope: {
+            rec: '='
+        },
+        restrict: 'E',
+        replace: 'true',
+        template: (
+            '<div class="recs">' +
+                '<h4>{{rec.name}}</h4>' +
+                '<h5>{{rec.type}}</h5>' +
+                '<a href="' + '{{rec.youtube}}' + '">Youtube Link</a>' +
+            '</div>'
+        ),
+    };
+    
+})
