f8f66469470f9f95656ea8aece709a7e152bf507
Eric Schofield <eschos24@gmail.com>
2018-10-19 02:37:17 +0000

    loop through to add recs items to array when api is called and returns

--- recs.js
+++ recs.js
@@ -7,26 +7,27 @@ app.controller('myCtrl',
   function($scope, $http) {
     $scope.recs = [];
     $scope.recsSearch = function(form) {
-      console.log(form);
       var baseURL = "https://tastedive.com/api/similar?k=321657-Angular2-0PYPIS84&verbose=1&q=";
       var separator = "%2C";
       var band = form.band.replace(/ /g, '+');
       var movie = form.movie.replace(/ /g, '+');
       var completedURL = baseURL + band + separator + movie;
-      console.log("API endpoint: " + completedURL);
+      // console.log("API endpoint: " + completedURL);
+      
       $http.jsonp(completedURL,{jsonpCallbackParam: 'callback'})
         .then(function(response) {
-          console.log(response);
-          console.log(response.data);
-          console.log(response.data.Similar);
-          console.log(response.data.Similar.Results);
           var results = response.data.Similar.Results;
-          $scope.recs.push({
-            name: results[0].Name,
-            type: results[0].Type,
-            youtube: results[0].yUrl
-          });
-          console.log($scope.recs);
+          
+          $scope.recs = [];
+          for (var i = 0; i < results.length; i++) {
+            $scope.recs.push({
+              name: results[i].Name,
+              type: results[i].Type,
+              youtube: results[i].yUrl
+            });
+          }
+          
+          console.log("added all recs" + $scope.recs);
         });
       };
     $scope.clicked = function() {
